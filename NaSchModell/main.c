6fb56627456c39d229f376e602d784c2e6c07207
Fritsch <danielw.fritsch@web.de>
2017-11-06 00:06:23 +0100

    Add main.c (HelloWorld Example)

---
+++ NaSchModell/main.c
@@ -0,0 +1,30 @@
+
+/***************************************
+ * @file main.c
+ * @brief
+ * @author
+ * @date
+ ***************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+ /*****************************************
+  * @fn main
+  * @brief
+  * @param void
+  * @return EXIT_SUCCESS
+  * @author
+  * @date
+  *****************************************/
+int main(void)
+{
+
+   printf("Hallo Welt\n");
+
+   _getch();
+
+   return EXIT_SUCCESS;
+}
