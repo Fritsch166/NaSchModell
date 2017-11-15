9934e8146777a9773d8f315b63b5be496970a79d
Fritsch <danielw.fritsch@web.de>
2017-11-15 20:30:22 +0100

    add colorschemes template for F1,F2,F3

--- NaSchModell/c-files/colorschemes.c
+++ NaSchModell/c-files/colorschemes.c
@@ -1,14 +1,47 @@
 #include "..\h-files\naschmodell.h"
 
 static bool bIsInit = false;
-struct colorschemes GLOBAL_ARRAY_COLOR_SCHEMES[9];
+
+struct colorschemes GLOBAL_ARRAY_COLOR_SCHEMES[MAX_COLOR_SCHEMES];
+
+COLOR f1_Converter(struct saveState* pState);
+COLOR f2_Converter(struct saveState* pState);
+COLOR f3_Converter(struct saveState* pState);
 
 void initGlobalColorSchemes(void)
 {
-   if (bIsInit == false)
+   if (bIsInit == true)
    {
-      bIsInit = true;
-
-      //TODO init color schemes
+      return;
    }
+
+   bIsInit = true;
+
+   {
+      struct colorschemes sF1 = {1, f1_Converter};
+      struct colorschemes sF2 = {2, f2_Converter};
+      struct colorschemes sF3 = {3, f3_Converter};
+
+      GLOBAL_ARRAY_COLOR_SCHEMES[1] = sF1;
+      GLOBAL_ARRAY_COLOR_SCHEMES[2] = sF2;
+      GLOBAL_ARRAY_COLOR_SCHEMES[3] = sF3;
+   }
+}
+
+COLOR f1_Converter(struct saveState* pState)
+{
+   //TODO
+   return NULL;
+}
+
+COLOR f2_Converter(struct saveState* pState)
+{
+   //TODO
+   return NULL;
+}
+
+COLOR f3_Converter(struct saveState* pState)
+{
+   //TODO
+   return NULL;
 }
